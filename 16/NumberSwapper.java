4ed59b845ac49c4cb413b148c2daa5cf63f8576b
Murat Göksel <murat.goxel@gmail.com>
2019-07-23 10:49:24 -0400

    Add a method which uses bit manipulation

--- 16/NumberSwapper.java
+++ 16/NumberSwapper.java
@@ -1,23 +1,34 @@
 import java.util.concurrent.ThreadLocalRandom;
 
 public class NumberSwapper {
+
+	// using arithmetic
+	public static void swap(int[] a) {
+		a[0] = a[1] - a[0];
+		a[1] = a[1] - a[0];
+		a[0] = a[0] - a[1];
+	}
+
+	// using bit manipulation
+	public static void swapBits(int[] a) {
+		a[0] = a[0] ^ a[1];
+		a[1] = a[0] ^ a[1];
+		a[0] = a[0] ^ a[1];
+	}
+
 	public static void main(String[] args) {
 		ThreadLocalRandom random = ThreadLocalRandom.current();
 		int a = 0, b = 0;
-		if (args.length > 0) {
+		if (args.length > 1) {
 			a = Integer.parseInt(args[0]);
 			b = Integer.parseInt(args[1]);
 		} else {
 			a = random.nextInt(); 
 			b = random.nextInt();
 		}
-		System.out.format("a = %d, b = %d%n", a, b);
-
-		// swap a with b without using extra variables
-		a = b - a;
-		b = b - a;
-		a = a + b;
-
-		System.out.format("a = %d, b = %d%n", a, b);
+		int[] numbers = {a, b};
+		System.out.format("a = %d, b = %d%n", numbers[0], numbers[1]);
+		swapBits(numbers);
+		System.out.format("a = %d, b = %d%n", numbers[0], numbers[1]);
 	}
 }
