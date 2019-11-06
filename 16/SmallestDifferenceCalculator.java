d3c0534369b1325aef280b044211cc1bf3622433
Murat Göksel <murat.goxel@gmail.com>
2019-11-06 11:29:58 -0500

    Correct algorithm and take difference bidirectionally

--- 16/SmallestDifferenceCalculator.java
+++ 16/SmallestDifferenceCalculator.java
@@ -2,6 +2,8 @@ import java.util.Arrays;
 
 public class SmallestDifferenceCalculator {
     public static void main(String[] args) {
+        /* First enter the number of elements in the first array, then enter 
+        the elements of first array followed by the elements of the second array. */
         int aLength = Integer.parseInt(args[0]);
         int[] a = new int[aLength];
         int[] b = new int[args.length - aLength - 1];
@@ -16,17 +18,15 @@ public class SmallestDifferenceCalculator {
         System.out.format("Smallest difference is %d%n", calculateSmallestDifference(a, b));
     }
 
+    /* Runs in O(n * log(n) + m * log(n)) time where n = a.length and m = b.length. */
     static int calculateSmallestDifference(int[] a, int[] b) {
         int diff = Integer.MAX_VALUE;
         Arrays.sort(a);
         Arrays.sort(b);
         for (int i = 0, j = 0; i < a.length && j < b.length;) {
-            int d = a[i] - b[j];
-            if (d < 0) i++;
-            else {
-                diff = Math.min(diff, d);
-                j++;
-            }
+            diff = Math.min(diff, Math.abs(a[i] - b[j]));
+            if (a[i] < b[j]) i++;
+            else j++;
         }
         return diff;
     }
