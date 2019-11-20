456896324f4ba7f27efa164b03dfb1b8696f698c
Murat Göksel <murat.goxel@gmail.com>
2019-11-20 12:24:40 -0500

    Use the equation of line instead of defining a line by 2 points

--- 16/BisectSquares.java
+++ 16/BisectSquares.java
@@ -3,19 +3,18 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class BisectSquares {
     public static void main(String[] args) {
-        double[] c = new double[8]; // coordinates
+        // double min = Double.parseDouble(args[0]);
+        // double max = Double.parseDouble(args[1]);
+        // Square square1 = createSquare(min, max);
+        // Square square2 = createSquare(min, max);
+        double[] c = new double[args.length];   // coordinates
         for (int i = 0; i < args.length; i++) {
             c[i] = Double.parseDouble(args[i]);
         }
-        double min = Double.parseDouble(args[0]);
-        double max = Double.parseDouble(args[1]);
-        ThreadLocalRandom random = ThreadLocalRandom.current();
-        Square square1 = new Square(new Point(random.nextDouble(min, max), random.nextDouble(min, max)), 
-                                    new Point(random.nextDouble(min, max), random.nextDouble(min, max)));
-        Square square2 = new Square(new Point(random.nextDouble(min, max), random.nextDouble(min, max)), 
-                                    new Point(random.nextDouble(min, max), random.nextDouble(min, max)));
-        Line halvingLine = calculateHalvingLine(square1, square2);
-        System.out.format("Line %s cuts the squares %s and %s in half.%n", halvingLine, square1, square2);
+        Square square1 = new Square(c[0], c[1], c[2], c[3]);
+        Square square2 = new Square(c[4], c[5], c[6], c[7]);
+        System.out.format("Square 1: %s%nSquare 2: %s%nLine %s cuts the above squares in half.%n", 
+            square1, square2, calculateHalvingLine(square1, square2));
     }
 
     // Returns Line that cuts both squares in half
@@ -26,53 +25,73 @@ public class BisectSquares {
         }
         Point center1 = square1.getCenterPoint();
         Point center2 = square2.getCenterPoint();
-        Square leftSquare;
-        Square rightSquare;
-        
-        return new Line(square1.getCenterPoint(), square2.getCenterPoint());
+        Line halvingLine = new Line(center1, center2);
+        return halvingLine;
     }
 
     static class Square {
-        Point bottomLeft;
-        Point topRight;
+        /* We assume edges of a square are parallel to y and x-axes, 
+         * so we can represent a square with four lines parallel to axes. */
+        double x1;
+        double x2;
+        double y1;
+        double y2;
 
-        public Square(Point bottomLeft, Point topRight) {
-            if (bottomLeft.equals(topRight)) {
-                throw new IllegalArgumentException("Points must be different to constitute a square.");
-            }
-            this.bottomLeft = bottomLeft;
-            this.topRight = topRight;
+        public Square(double x1, double x2, double y1, double y2) {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
         }
 
         Point getCenterPoint() {
-            return new Point((bottomLeft.x + topRight.x) / 2.0,
-                             (bottomLeft.y + topRight.y) / 2.0);
+            return new Point((x1 + x2)/2.0, (y1 + y2)/2.0);
         }
 
         @Override
         public String toString() {
-            return String.format("[%s %s]", bottomLeft, topRight);
+            return String.format("[x1 = %f, x2 = %f, y1 = %f, y2 = %f]", x1, x2, y1, y2);
         }
     }
 
-    static class Line {
-        Point point1;
-        Point point2;
+    private static Square createSquare(double min, double max) {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        double x1 = random.nextDouble(min, max);
+        double x2 = random.nextDouble(x1, max);
+        double y1 = random.nextDouble(min, max);
+        double y2 = random.nextDouble(y1, max);
+        return new Square(x1, x2, y1, y2);
+    }
 
-        public Line(Point p1, Point p2) {
-            point1 = p1;
-            if (p1.equals(p2)) {
-                // There are infinitely many line passing through this point.
-                // Let's pick one.
-                point2 = new Point(0, 0);
+    static class Line {
+        double slope;
+        double y_intercept;
+        double x_intercept;
+
+        public Line(Point point1, Point point2) {
+            double delta_y = point2.y - point1.y;
+            double delta_x = point2.x - point1.x;
+            if (delta_x == 0.0) {   // line is parallel to y-axis
+                slope = Double.NaN;
+                x_intercept = point1.x;
             } else {
-                point2 = p2;
+                slope = delta_y / delta_x;
+                y_intercept = point1.y - slope * point1.x;
             }
+
+        }
+
+        boolean isParallelToYAxis() {
+            return Double.isNaN(slope);
         }
 
         @Override
         public String toString() {
-            return String.format("%s--%s", point1, point2);
+            if (isParallelToYAxis()) {
+                return "x = " + x_intercept;
+            } else {
+                return String.format("y = %fx + %f", slope, y_intercept);
+            }
         }
     }
 
