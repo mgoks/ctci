8223c4ec99235f766c665e88bc7ad25eeee82818
Murat Göksel <murat.goxel@gmail.com>
2019-12-16 17:08:23 -0500

    Add new method removeKey\nUpdate access modifiers

--- 16/LRUCache.java
+++ 16/LRUCache.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 // designed to pass https://leetcode.com/problems/lru-cache
 // all operations take constant time
 // O(n) space in capacity of the cache
-class LRUCache {
-    HashMap<Integer, LinkedListNode> nodes = new HashMap<>();    // key to node mappings
-    LinkedListNode dummyHead = new LinkedListNode(0, 0);
-    LinkedListNode dummyTail = new LinkedListNode(0, 0);
-    int maxSize;
+public class LRUCache {
+    private HashMap<Integer, LinkedListNode> nodes = new HashMap<>();    // key to node mappings
+    private LinkedListNode dummyHead = new LinkedListNode(0, 0);
+    private LinkedListNode dummyTail = new LinkedListNode(0, 0);
+    private int maxSize;
     
     public LRUCache(int capacity) {
         maxSize = capacity;
@@ -16,6 +16,7 @@ class LRUCache {
         dummyTail.prev = dummyHead;
     }
     
+    // returns the value associated with key
     public int get(int key) {
         if (!nodes.containsKey(key))
             return -1;
@@ -25,24 +26,25 @@ class LRUCache {
         return node.value;
     }
     
+    // inserts key value pair to the cache
     public void put(int key, int value) {
-        if (nodes.containsKey(key)) {
-            LinkedListNode node = nodes.get(key);
-            node.value = value; // value might not equal node.value
-            removeNode(node);
-            makeHead(node);
-            return;
-        }
-        if (nodes.size() == maxSize) {
-            LinkedListNode tail = getTail();
-            nodes.remove(tail.key);
-            removeNode(tail);
-        }
+        if (nodes.containsKey(key)) 
+            removeKey(key);
+        if (nodes.size() == maxSize)   // remove tail.key from cache
+            removeKey(getTail().key);
         LinkedListNode node = new LinkedListNode(key, value);
         makeHead(node);
         nodes.put(key, node);
     }
+
+    // removes key and its associated value from cache
+    private void removeKey(int key) {
+        LinkedListNode node = nodes.get(key);
+        removeNode(node);
+        nodes.remove(key);
+    }
     
+    // makes head the first element in the internal linked list
     private void makeHead(LinkedListNode head) {
         if (head == null)
             return;
@@ -53,13 +55,15 @@ class LRUCache {
         head.prev = dummyHead;
     }
 
-    // the node least recently used key
+    // returns the last element in the internal linked list,
+    // i.e. the least recently used element
     private LinkedListNode getTail() {
         if (dummyTail.prev == null || dummyTail.prev == dummyHead)
             return null;
         return dummyTail.prev;
     }
 
+    // removes node from the internal linked list
     private void removeNode(LinkedListNode node) {
         if (node == null || node == dummyHead || node == dummyTail)
             return;
@@ -70,11 +74,11 @@ class LRUCache {
     }
 
     
-    class LinkedListNode {
+    private class LinkedListNode {
         int key;
         int value;
-        LinkedListNode next = null;
-        LinkedListNode prev = null;
+        private LinkedListNode next = null;
+        private LinkedListNode prev = null;
 
         public LinkedListNode(int key, int val) {
             this.key = key;
@@ -82,10 +86,3 @@ class LRUCache {
         }
     }
 }
-
-/**
- * The LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
