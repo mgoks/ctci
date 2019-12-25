fce0b7acf4c375d90264b04b70e55d01d4ef6e7f
Murat Göksel <murat.goxel@gmail.com>
2019-12-25 18:33:55 -0500

    Use custom BitInteger class instead of int

--- 17/MissingIntegerFinder.java
+++ 17/MissingIntegerFinder.java
@@ -7,14 +7,14 @@ public class MissingIntegerFinder {
         int n = ThreadLocalRandom.current().nextInt(0, 1000000);
         int missingInt = ThreadLocalRandom.current().nextInt(0, n+1);
         System.out.println("Missing integer is " + missingInt);
-        int[] a = new int[n];
+        BitInteger[] a = new BitInteger[n];
         int offset = 0;
         for (int i = 0; i <= n; i++) {
             if (i == missingInt) {
                 offset = 1;
                 continue;
             }
-            a[i - offset] = i;
+            a[i - offset] = new BitInteger(i);
         }
         finder.shuffle(a);
         // System.out.println("A = " + Arrays.toString(a));
@@ -22,7 +22,7 @@ public class MissingIntegerFinder {
     }
 
     // all a[i] is non-negative
-    int findMissingInteger(int[] a) {
+    int findMissingInteger(BitInteger[] a) {
         if (a == null)
             return -1;
         int n = a.length;
@@ -32,27 +32,40 @@ public class MissingIntegerFinder {
             n_xor ^= i;
         for (int j = 0; j < Integer.SIZE; j++) {
             int bit = 0;
-            for (int i : a)
-                bit ^= fetchBit(j, i);  // can't access a[i] directly according to problem description
-            a_xor = a_xor | (bit << j); // set jth bit of a_xor to bit
+            for (BitInteger bitInteger : a)
+                bit ^= bitInteger.fetchBit(j);  // can't access a[i] directly according to problem description
+            a_xor |= (bit << j); // set jth bit of a_xor to bit
         }
         return a_xor ^ n_xor;
     }
 
-    // sig ... significance
-    private int fetchBit(int sig, int integer) {
-        // System.out.format("fetchBit(%d, %s) = %d%n", sig, Integer.toBinaryString(integer), (integer>>sig)&1);
-        return (integer >> sig) & 1;
-    }
-
-    private void shuffle(int[] a) {
+    private void shuffle(BitInteger[] a) {
         if (a == null)
             return;
         for (int i = 0; i < a.length; i++) {
             int j = ThreadLocalRandom.current().nextInt(0, i+1);
-            int temp = a[i];
+            BitInteger temp = a[i];
             a[i] = a[j];
             a[j] = temp;
+        }
+    }
+
+
+    static class BitInteger {
+        private int value;
+
+        BitInteger(int v) {
+            value = v;
+        }
+
+        // sig ... significance
+        int fetchBit(int sig) {
+            return (value >> sig) & 1;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
         }
     }
 }
